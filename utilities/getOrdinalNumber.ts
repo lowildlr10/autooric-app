3471f24b0605faf6f40e01c182c760e01e552557
Lowil Ray Delos Reyes <lowildlr10@gmail.com>
2024-04-29 19:18:30 +0800

    Re-add the categories tab on libraries; Added the ordinal number generator;

---
+++ utilities/getOrdinalNumber.ts
@@ -0,0 +1,10 @@
+
+const getOrdinalNumber = (intValue: number) => {
+    if (intValue === 0) return "Not set";
+
+    const suffixes = ["th", "st", "nd", "rd"];
+    const v = intValue % 100;
+    return intValue + (suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0]);
+}
+
+export default getOrdinalNumber
